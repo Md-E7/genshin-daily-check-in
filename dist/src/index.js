894a2cc6cb090a96fd5da826bb48f507c0d252b2
Md-E7 <madeegi7@gmail.com>
2023-02-13 15:23:08 +0800

    :package: run npm build

--- dist/src/index.js
+++ dist/src/index.js
@@ -2,8 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_json_1 = require("../config.json");
 const ofetch_1 = require("ofetch");
+const discord_js_1 = require("discord.js");
+const webhookClient = config_json_1.discord_webhook_url.length !== 0 ? new discord_js_1.WebhookClient({ url: config_json_1.discord_webhook_url }) : null;
+const sendWebHookMessage = async (message) => {
+    return await webhookClient?.send({
+        content: `> **${message}**`
+    });
+};
 const checkIn = async () => {
-    const response = await (0, ofetch_1.ofetch)(' https://sg-hk4e-api.hoyolab.com/event/sol/sign', {
+    return await (0, ofetch_1.ofetch)(' https://sg-hk4e-api.hoyolab.com/event/sol/sign', {
         method: 'POST',
         query: {
             act_id: config_json_1.act_id
@@ -11,18 +18,10 @@ const checkIn = async () => {
         headers: {
             cookie: config_json_1.cookie
         }
-    }).catch(reason => {
-        throw new Error(reason);
     });
-    if (response.message === 'OK') {
-        console.info(response);
-        return true;
-    }
-    console.error(response);
-    return false;
 };
 const reCheckIn = async () => {
-    const response = await (0, ofetch_1.ofetch)(' https://sg-hk4e-api.hoyolab.com/event/sol/resign', {
+    return await (0, ofetch_1.ofetch)(' https://sg-hk4e-api.hoyolab.com/event/sol/resign', {
         method: 'POST',
         query: {
             act_id: config_json_1.act_id
@@ -30,21 +29,21 @@ const reCheckIn = async () => {
         headers: {
             cookie: config_json_1.cookie
         }
+    });
+};
+const init = () => {
+    void checkIn().then(async (response) => {
+        console.info(response.message);
+        await sendWebHookMessage(response.message);
     }).catch(reason => {
         throw new Error(reason);
     });
-    if (response.message === 'OK') {
-        console.info(response);
-        return true;
-    }
-    console.error(response);
-    return false;
-};
-const init = () => {
-    void checkIn();
-    setTimeout(() => {
-        void reCheckIn();
-    }, 3 * 1000);
+    void reCheckIn().then(async (response) => {
+        console.info(response.message);
+        await sendWebHookMessage(response.message);
+    }).catch(reason => {
+        throw new Error(reason);
+    });
 };
 init();
 setInterval(init, 24 * 60 * 60 * 1000);
