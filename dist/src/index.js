f40ae9b01deb383d406d499a50762b1069d63ddc
Md-E7 <madeegi7@gmail.com>
2023-02-14 14:06:20 +0800

    :package: run npm build

--- dist/src/index.js
+++ dist/src/index.js
@@ -10,79 +10,81 @@ const sendWebHookMessage = async (message) => {
         content: `> **${message}**`
     });
 };
-const checkIn = async () => {
+const checkIn = async (act_id, cookie) => {
     return await (0, ofetch_1.ofetch)(' https://sg-hk4e-api.hoyolab.com/event/sol/sign', {
         method: 'POST',
         query: {
-            act_id: config_json_1.act_id
+            act_id
         },
         headers: {
-            cookie: config_json_1.cookie
+            cookie
         }
     });
 };
-const completeTask = async (id) => {
+const completeTask = async (id, act_id, cookie) => {
     return await (0, ofetch_1.ofetch)('https://sg-hk4e-api.hoyolab.com/event/sol/task/complete', {
         method: 'POST',
         query: {
-            act_id: config_json_1.act_id,
+            act_id,
             id
         },
         headers: {
-            cookie: config_json_1.cookie
+            cookie
         }
     });
 };
-const claimAward = async (id) => {
+const claimAward = async (id, act_id, cookie) => {
     return await (0, ofetch_1.ofetch)('https://sg-hk4e-api.hoyolab.com/event/sol/task/award', {
         method: 'POST',
         query: {
-            act_id: config_json_1.act_id,
+            act_id,
             id
         },
         headers: {
-            cookie: config_json_1.cookie
+            cookie
         }
     });
 };
-const reCheckIn = async () => {
+const reCheckIn = async (act_id, cookie) => {
     return await (0, ofetch_1.ofetch)(' https://sg-hk4e-api.hoyolab.com/event/sol/resign', {
         method: 'POST',
         query: {
-            act_id: config_json_1.act_id
+            act_id
         },
         headers: {
-            cookie: config_json_1.cookie
+            cookie
         }
     });
 };
 const init = async () => {
-    const checkInResponse = await checkIn().catch(reason => {
-        throw new Error(reason);
-    });
-    console.info(`[Check In]: ${JSON.stringify(checkInResponse)}`);
-    void sendWebHookMessage(checkInResponse.message);
-    await delay(3 * 1000);
-    for (let i = 1; i <= 3; i++) {
+    for (const account of config_json_1.accounts) {
+        const checkInResponse = await checkIn(account.act_id, account.cookie).catch(reason => {
+            throw new Error(reason);
+        });
+        console.info(`[Check In]: ${JSON.stringify(checkInResponse)}`);
+        void sendWebHookMessage(`[${account.name}] ${checkInResponse.message}`);
         await delay(3 * 1000);
-        const completeTaskResponse = await completeTask(i).catch(reason => {
+        for (let i = 1; i <= 3; i++) {
+            await delay(3 * 1000);
+            const completeTaskResponse = await completeTask(i, account.act_id, account.cookie).catch(reason => {
+                throw new Error(reason);
+            });
+            console.info(`[Complete Task]: ${JSON.stringify(completeTaskResponse)}`);
+            const claimAwardResponse = await claimAward(i, account.act_id, account.cookie).catch(reason => {
+                throw new Error(reason);
+            });
+            console.info(`[Claim Award]: ${JSON.stringify(claimAwardResponse)}`);
+        }
+        await delay(3 * 1000);
+        const reCheckInResponse = await reCheckIn(account.act_id, account.cookie).catch(reason => {
             throw new Error(reason);
         });
-        console.info(`[Complete Task]: ${JSON.stringify(completeTaskResponse)}`);
-        const claimAwardResponse = await claimAward(i).catch(reason => {
-            throw new Error(reason);
-        });
-        console.info(`[Claim Award]: ${JSON.stringify(claimAwardResponse)}`);
+        console.info(`[Re Check In]: ${JSON.stringify(reCheckInResponse)}`);
+        void sendWebHookMessage(`[${account.name}] ${reCheckInResponse.message}`);
+        await delay(3 * 1000);
+        console.log('Auto daily check in will be repeat in 24 hours');
+        void sendWebHookMessage(`[${account.name}] Genshin impact auto daily check in will be repeat in 24 hours`);
     }
-    await delay(3 * 1000);
-    const reCheckInResponse = await reCheckIn().catch(reason => {
-        throw new Error(reason);
-    });
-    console.info(`[Re Check In]: ${JSON.stringify(reCheckInResponse)}`);
-    void sendWebHookMessage(reCheckInResponse.message);
-    await delay(3 * 1000);
-    console.log('Auto daily check in will be repeat in 24 hours');
-    void sendWebHookMessage('Genshin impact auto daily check in will be repeat in 24 hours');
 };
 void init();
 setInterval(() => {
