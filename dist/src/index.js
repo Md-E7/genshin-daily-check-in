14378f5bd66bcc93864deea5a4fc1987dd1f977f
Md-E7 <madeegi7@gmail.com>
2023-02-26 18:04:35 +0800

    :bug: Fix MODULE_NOT_FOUND when using npm run start

--- dist/src/index.js
+++ dist/src/index.js
@@ -2,14 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_json_1 = require("../config.json");
 const ofetch_1 = require("ofetch");
-const discord_js_1 = require("discord.js");
-const webhookClient = config_json_1.discord_webhook_url.length !== 0 ? new discord_js_1.WebhookClient({ url: config_json_1.discord_webhook_url }) : null;
 const delay = async (ms) => await new Promise(resolve => setTimeout(resolve, ms));
-const sendWebHookMessage = async (message) => {
-    return await webhookClient?.send({
-        content: `> **${message}**`
-    });
-};
 const checkIn = async (act_id, cookie) => {
     return await (0, ofetch_1.ofetch)(' https://sg-hk4e-api.hoyolab.com/event/sol/sign', {
         method: 'POST',
@@ -61,29 +54,26 @@ const init = async () => {
         const checkInResponse = await checkIn(account.act_id, account.cookie).catch(reason => {
             throw new Error(reason);
         });
-        console.info(`[Check In]: ${JSON.stringify(checkInResponse)}`);
-        void sendWebHookMessage(`[${account.name}] ${checkInResponse.message}`);
-        await delay(3 * 1000);
+        console.info(`[${account.name}] Check-in: ${JSON.stringify(checkInResponse)}`);
+        await delay(500);
         for (let i = 1; i <= 3; i++) {
-            await delay(3 * 1000);
+            await delay(500);
             const completeTaskResponse = await completeTask(i, account.act_id, account.cookie).catch(reason => {
                 throw new Error(reason);
             });
-            console.info(`[Complete Task]: ${JSON.stringify(completeTaskResponse)}`);
+            console.info(`[${account.name}] Complete Task: ${JSON.stringify(completeTaskResponse)}`);
             const claimAwardResponse = await claimAward(i, account.act_id, account.cookie).catch(reason => {
                 throw new Error(reason);
             });
-            console.info(`[Claim Award]: ${JSON.stringify(claimAwardResponse)}`);
+            console.info(`[${account.name}] Claim Award: ${JSON.stringify(claimAwardResponse)}`);
         }
-        await delay(3 * 1000);
+        await delay(500);
         const reCheckInResponse = await reCheckIn(account.act_id, account.cookie).catch(reason => {
             throw new Error(reason);
         });
-        console.info(`[Re Check In]: ${JSON.stringify(reCheckInResponse)}`);
-        void sendWebHookMessage(`[${account.name}] ${reCheckInResponse.message}`);
-        await delay(3 * 1000);
-        console.log('Auto daily check in will be repeat in 24 hours');
-        void sendWebHookMessage(`[${account.name}] Genshin impact auto daily check in will be repeat in 24 hours`);
+        console.info(`[${account.name}] Re-check-in: ${JSON.stringify(reCheckInResponse)}`);
+        await delay(500);
+        console.info(`[${account.name}] Genshin impact auto daily check in will be repeat in 24 hours`);
     }
 };
 void init();
